67a0ab4dbe4f696ffa64b73205f2c5568381183f
Jayasree Challa <jayasree_challa@merilytics.com>
2021-07-22 18:40:50 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  profileForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+  });
+
+
+  constructor(private fb: FormBuilder) { }
+ 
+  title = 'Merilytics';
+  name = new FormControl('');
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  
+}
